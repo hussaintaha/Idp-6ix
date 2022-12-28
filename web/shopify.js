17f50c3cb7ca87c491119d9c09da7388c7ea4c0d
Hussain Taha <hussaintaha500@gmail.com>
2022-12-28 11:50:10 +0530

    Added production variables

--- web/shopify.js
+++ web/shopify.js
@@ -24,6 +24,10 @@ const billingConfig = {
 };
 
 const shopify = shopifyApp({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: process.env.SCOPES,
+  hostName: process.env.HOST,
   api: {
     apiVersion: LATEST_API_VERSION,
     restResources,
